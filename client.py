b72edaa571fa9b8c20999cdaba6440ab62617e01
Furqan Shakoor <furqan.shakoor@gmail.com>
2019-03-26 03:01:26 +0100

    Adapted for pubsub tests

--- client.py
+++ client.py
@@ -1,18 +1,20 @@
 import asyncio
+import random
+
 import websockets
 
 
-async def hello():
-    websocket = await websockets.connect("ws://localhost:8765")
-    for _ in range(1):
-        await websocket.send("ping")
-
-        # response = await websocket.recv()
-        # print(f"Got response {response}")
+async def hello(task_name, task_number):
+    port = random.choice([8765, 8766])
+    websocket = await websockets.connect(f"ws://localhost:{port}")
+    while True:
+        response = await websocket.recv()
+        ident = ">  " * task_number
+        print(f"{ident}({task_name}):{response}")
 
 
 tasks = []
-for _ in range(10):
-    tasks.append(hello())
+for i in range(4):
+    tasks.append(hello(f"task_{i}", i))
 
 asyncio.get_event_loop().run_until_complete(asyncio.wait(tasks))
