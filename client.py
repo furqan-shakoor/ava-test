e0adec78362f252c10444bd8d1d25e8512ca82be
Furqan Shakoor <furqan.shakoor@gmail.com>
2019-05-30 21:30:31 +0200

    cloud test

--- client.py
+++ client.py
@@ -5,16 +5,17 @@ import websockets
 
 
 async def hello(task_name, task_number):
-    port = random.choice([8765, 8766])
-    websocket = await websockets.connect(f"ws://localhost:{port}")
+    ip = random.choice(["139.59.136.182", "165.22.89.205"])
+    port = 80
+    websocket = await websockets.connect(f"ws://{ip}:{port}")
+    # await websocket.send("ping")
     while True:
         response = await websocket.recv()
-        ident = ">  " * task_number
-        print(f"{ident}({task_name}):{response}")
+        print(f"({task_name}):{response}")
 
 
 tasks = []
-for i in range(4):
+for i in range(3000):
     tasks.append(hello(f"task_{i}", i))
 
 asyncio.get_event_loop().run_until_complete(asyncio.wait(tasks))
