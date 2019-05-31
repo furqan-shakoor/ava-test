35d3eb627cb09f743ec72ae86a420b30705dcf74
Furqan Shakoor <furqan.shakoor@gmail.com>
2019-05-31 20:28:15 +0200

    Recording data

--- client.py
+++ client.py
@@ -1,21 +1,39 @@
 import asyncio
 import random
+from datetime import datetime
 
 import websockets
+
+conn_times = []
+
+
+def write_conn_times():
+    with open('conn_times.txt', 'w') as f:
+        for conn_time in conn_times:
+            f.write(f"{str(conn_time.timestamp())}, 1\n")
 
 
 async def hello(task_name, task_number):
     ip = random.choice(["139.59.136.182", "165.22.89.205"])
     port = 80
     websocket = await websockets.connect(f"ws://{ip}:{port}")
+    conn_times.append(datetime.now())
     # await websocket.send("ping")
     while True:
         response = await websocket.recv()
         print(f"({task_name}):{response}")
 
 
-tasks = []
-for i in range(3000):
-    tasks.append(hello(f"task_{i}", i))
+def main():
+    tasks = []
+    for i in range(10):
+        tasks.append(hello(f"task_{i}", i))
 
-asyncio.get_event_loop().run_until_complete(asyncio.wait(tasks))
+    try:
+        asyncio.get_event_loop().run_until_complete(asyncio.wait(tasks))
+    except KeyboardInterrupt:
+        write_conn_times()
+
+
+if __name__ == "__main__":
+    main()
